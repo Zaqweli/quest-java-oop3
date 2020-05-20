6b195ea6d43338ac7c49068a89aebcac3cbb81c7
xabi <xavier.guezala@live.fr>
2020-05-20 12:04:19 +0200

    Ajout de Fly

--- Eagle.java
+++ Eagle.java
@@ -1,7 +1,11 @@
-public class Eagle extends Bird {
+public class Eagle extends Bird implements Fly {
 
     private boolean flying;
     private int altitude;
+
+    public void setAltitude(int altitude) {
+        this.altitude = altitude;
+    }
 
     public Eagle(String name) {
         super(name);
@@ -21,4 +25,46 @@ public class Eagle extends Bird {
     public String sing() {
         return "Screech!";
     }
+
+    @Override
+    public void takeOff() {
+        System.out.println("TakeOff!");
+    }
+
+    @Override
+    public int ascend(int meters) {
+        System.out.println(this.getName() + " ascending " + meters);
+        this.setAltitude(getAltitude() + meters);
+        System.out.println("Height is " + this.getAltitude());
+        return this.getAltitude();
+    }
+
+    @Override
+    public int descend(int meters) {
+        System.out.println(this.getName() + " descending " + meters);
+        if (this.getAltitude() < meters) {
+            System.out.println("Height is " + this.getAltitude());
+            System.out.println("Hawkeye cannot descend any more!");
+        }
+        else {
+            this.setAltitude(this.getAltitude() - meters);
+            System.out.println("Height is " + this.getAltitude());
+        }
+        return this.getAltitude();
+    }
+
+    @Override
+    public void land () {
+        System.out.println("Hawkeye lands on the ground");
+        this.setAltitude(0);
+    }
+
+    @Override
+    public void glide () {
+        System.out.println("Hawkeye glides into the air.");
+    }
+
+
+
 }
+
